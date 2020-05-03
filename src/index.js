f6ad8ecbc89c7a65600f9fba8e9e21448d7c6a70
Pablo Palacios <pablo.palacios@moplin.com>
2020-05-03 13:17:22 -0500

    2

--- src/index.js
+++ src/index.js
@@ -14,22 +14,14 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-
           {utils.range(1, stars).map(starId =>
-            <div className="star" />
+            <div key={starId} className="star" />
           )}
-
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {utils.range(1, stars).map(number =>
+            <button key={number} className="number">{number}</button>
+          )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
@@ -86,14 +78,11 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+
+        <StarMatch />
+
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<StarMatch />, document.getElementById('root'));
